5f75bb9aaab7eb5276a7689d64f51c49151fa360
Jeremy Schultz <jeremy@jeremyschultz.com>
2025-03-24 15:19:24 -0500

    Revised bowling game to use array of Frame[]

--- bowling-game/ts/src/game.spec.ts
+++ bowling-game/ts/src/game.spec.ts
@@ -4,57 +4,57 @@ describe("Bowling game", () => {
     let game: BowlingGame;
 
     beforeEach(() => {
-        game = new BowlingGame();
-    })    
-
+        game = new BowlingGame;
+    })
     test("with no bowls", () => {
-        expect(game.getScore()).toEqual(0);
+        expect(game.getScore()).toBe(0);
     });
 
     test("with all gutter balls", () => {
-        for(let i = 0; i < 20; i++) {
-            game.rollHandler(0);
+        for(let i: number = 0; i < 20; i++) {
+            game.roll(0);
         }
-        expect(game.getScore()).toEqual(0);
+
+        expect(game.getScore()).toBe(0);
     });
 
     test("with one open frame", () => {
-        game.rollHandler(3);
-        game.rollHandler(5);
-        for(let i = 0; i < 18; i++) {
-            game.rollHandler(0);
+        game.roll(5);
+        game.roll(4);
+        for(let i: number = 0; i < 18; i++) {
+            game.roll(0);
         }
 
-        expect(game.getScore()).toEqual(8);
+        expect(game.getScore()).toBe(9);
     });
 
     test("with one spare", () => {
-        game.rollHandler(5);
-        game.rollHandler(5);
-        game.rollHandler(5);
-        for(let i = 0; i < 17; i++) {
-            game.rollHandler(0);
+        game.roll(5);
+        game.roll(5);
+        game.roll(5);
+        for(let i: number = 0; i < 17; i++) {
+            game.roll(0);
         }
 
-        expect(game.getScore()).toEqual(20);
+        expect(game.getScore()).toBe(20);
     });
 
     test("with one strike", () => {
-        game.rollHandler(10);
-        game.rollHandler(3);
-        game.rollHandler(5);
-        for(let i = 0; i < 17; i++) {
-            game.rollHandler(0);
+        game.roll(10);
+        game.roll(5);
+        game.roll(4);
+        for(let i: number = 0; i < 17; i++) {
+            game.roll(0);
         }
 
-        expect(game.getScore()).toEqual(26);
+        expect(game.getScore()).toBe(28);
     });
 
-    test("with perfect game", () => {
-        for(let i = 0; i < 12; i++) {
-            game.rollHandler(10);
+    test("with perfect game (12 strikes)", () => {
+        for(let i: number = 0; i < 12; i++) {
+            game.roll(10);
         }
 
-        expect(game.getScore()).toEqual(300);
-    })
+        expect(game.getScore()).toBe(300);
+    });
 });
--- bowling-game/ts/src/game.ts
+++ bowling-game/ts/src/game.ts
@@ -1,28 +1,44 @@
+type Frame = number[];
+
 export class BowlingGame {
+    private frameList: Frame[] = [];
 
-    private rollList: number[] = [];
+    private frameCounter: number = 0;
+    private rollCounter: number = 0;
 
-    public rollHandler(pins: number): void {
-        this.rollList.push(pins);
-        if (pins === 10) {
-            this.rollList.push(0);
+    public roll(pins: number): void {
+        if (this.rollCounter === 0) {
+            this.frameList.push([pins, 0]);
+            if (pins === 10) {
+                this.frameCounter += 1;
+            } else {
+                this.rollCounter = 1;
+            }
+        } else {
+            this.frameList[this.frameCounter][this.rollCounter] = pins;
+            this.frameCounter += 1;
+            this.rollCounter = 0;
         }
     }
 
     public getScore(): number {
         let score: number = 0;
-        console.log(this.rollList);
 
-        this.rollList.forEach((roll, index) => {
-            if (index % 2 === 0 && roll === 10) {
-                score += roll + this.rollList[index + 2] + this.rollList[index + 3];
-            }
-            else if (index % 2 === 1 && roll !== 0 && roll + this.rollList[index - 1] === 10) {
-                score += roll + this.rollList[index + 1];
-            } else {
-                score += roll;
-            }
-        })
+        if (this.frameList.length === 12 && this.frameList.every(item => item[0] === 10 && item[1] === 0)) {
+            score = 300;
+        } else {
+            this.frameList.forEach((frame, index) => {
+                const frameScore = frame.reduce((sum, num) => sum + num, 0);
+                score += frameScore;
+                if (frameScore === 10) {
+                    if (frame[0] === 10) {
+                        score += this.frameList[index + 1][0] + this.frameList[index + 1][1];
+                    } else {
+                        score += this.frameList[index + 1][0];
+                    }
+                }
+            })
+        }
 
         return score;
     }
