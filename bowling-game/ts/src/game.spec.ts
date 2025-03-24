8b864a29bc0bfa92fcb8369037b0cd6eb5dd54e2
Jeremy Schultz <jeremy@jeremyschultz.com>
2025-03-24 13:31:33 -0500

    Initial work on bowling game, now rolling back to start again

--- bowling-game/ts/src/game.spec.ts
+++ bowling-game/ts/src/game.spec.ts
@@ -1,5 +1,60 @@
-import {foo} from "./game";
+import {BowlingGame} from "./game";
 
-test('something', () => {
-    expect(foo()).toEqual(false);
+describe("Bowling game", () => {
+    let game: BowlingGame;
+
+    beforeEach(() => {
+        game = new BowlingGame();
+    })    
+
+    test("with no bowls", () => {
+        expect(game.getScore()).toEqual(0);
+    });
+
+    test("with all gutter balls", () => {
+        for(let i = 0; i < 20; i++) {
+            game.rollHandler(0);
+        }
+        expect(game.getScore()).toEqual(0);
+    });
+
+    test("with one open frame", () => {
+        game.rollHandler(3);
+        game.rollHandler(5);
+        for(let i = 0; i < 18; i++) {
+            game.rollHandler(0);
+        }
+
+        expect(game.getScore()).toEqual(8);
+    });
+
+    test("with one spare", () => {
+        game.rollHandler(5);
+        game.rollHandler(5);
+        game.rollHandler(5);
+        for(let i = 0; i < 17; i++) {
+            game.rollHandler(0);
+        }
+
+        expect(game.getScore()).toEqual(20);
+    });
+
+    test("with one strike", () => {
+        game.rollHandler(10);
+        game.rollHandler(3);
+        game.rollHandler(5);
+        for(let i = 0; i < 17; i++) {
+            game.rollHandler(0);
+        }
+
+        expect(game.getScore()).toEqual(26);
+    });
+
+    test("with perfect game", () => {
+        for(let i = 0; i < 12; i++) {
+            game.rollHandler(10);
+        }
+
+        expect(game.getScore()).toEqual(300);
+    })
 });
--- bowling-game/ts/src/game.ts
+++ bowling-game/ts/src/game.ts
@@ -1,3 +1,29 @@
-export function foo() {
-    return "bar"
+export class BowlingGame {
+
+    private rollList: number[] = [];
+
+    public rollHandler(pins: number): void {
+        this.rollList.push(pins);
+        if (pins === 10) {
+            this.rollList.push(0);
+        }
+    }
+
+    public getScore(): number {
+        let score: number = 0;
+        console.log(this.rollList);
+
+        this.rollList.forEach((roll, index) => {
+            if (index % 2 === 0 && roll === 10) {
+                score += roll + this.rollList[index + 2] + this.rollList[index + 3];
+            }
+            else if (index % 2 === 1 && roll !== 0 && roll + this.rollList[index - 1] === 10) {
+                score += roll + this.rollList[index + 1];
+            } else {
+                score += roll;
+            }
+        })
+
+        return score;
+    }
 }
