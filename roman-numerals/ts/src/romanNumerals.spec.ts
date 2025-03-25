917736a27e82e2a6ffa5680baafe2bdf550fa5a2
Jeremy Schultz <jeremy@jeremyschultz.com>
2025-03-25 12:35:11 -0500

    Initial work on convertToRomanNumeral()

--- roman-numerals/ts/src/romanNumerals.spec.ts
+++ roman-numerals/ts/src/romanNumerals.spec.ts
@@ -1,5 +1,44 @@
-import {foo} from "./romanNumerals";
+import {convertToRomanNumeral, convertToInteger} from "./romanNumerals";
 
-test('something', () => {
-    expect(foo()).toEqual(false);
+describe("convertToRomanNumeral", () => {
+    test("1 to be I", () => {
+        expect(convertToRomanNumeral(1)).toEqual("I");
+    });
+    test("2 to be II", () => {
+        expect(convertToRomanNumeral(2)).toEqual("II");
+    });
+    test("3 to be III", () => {
+        expect(convertToRomanNumeral(3)).toEqual("III");
+    });
+    test("4 to be IV", () => {
+        expect(convertToRomanNumeral(4)).toEqual("IV");
+    });
+
+    test("5 to be V", () => {
+        expect(convertToRomanNumeral(5)).toEqual("V");
+    });
 });
+
+
+// Roman Numeral Conversion Chart:
+// 1 -> I
+// 2 -> II
+// 3 -> III
+// 4 -> IV
+// 5 -> V
+// 9 -> IX
+// 10 -> X
+// 20 -> XX
+// 40 -> XL
+// 50 -> L
+// 90 -> XC
+// 100 -> C
+// 400 -> CD
+// 500 -> D
+// 900 -> CM
+// 1000 -> M
+
+
+
+
+
--- roman-numerals/ts/src/romanNumerals.ts
+++ roman-numerals/ts/src/romanNumerals.ts
@@ -1,3 +1,19 @@
-export function foo() {
-    return "bar"
+export function convertToRomanNumeral(input: number): string {
+    let result: string = "";
+
+    if (input >= 5) {
+        return "V";
+    } else {
+        for(let i = 0; i < input; i++) {
+            result += "I";
+        }
+        if (result.indexOf("IIII") !== -1) {
+            result = "IV";
+        }
+    }
+    return result;
+}
+
+export function convertToInteger(input: string): number {
+    return 3;
 }
