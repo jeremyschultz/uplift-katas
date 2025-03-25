3a333fa8918a4323b433a506858d1b94f42565cb
Jeremy Schultz <jeremy@jeremyschultz.com>
2025-03-25 10:39:28 -0500

    Wrote fizzbuzz and unit tests

--- fizzbuzz/ts/src/fizzbuzz.spec.ts
+++ fizzbuzz/ts/src/fizzbuzz.spec.ts
@@ -1,5 +1,31 @@
 import {fizzbuzz} from "./fizzbuzz";
 
-test('something', () => {
-    expect(fizzbuzz(1)).toEqual(false)
+describe("fizzbuzz", () => {
+    test("does not go past 100", () => {
+        expect(fizzbuzz(100)).not.toContain("101\n");
+    });
+
+    test("prints numbers 1-100 with newlines", () => {
+        for (let i of [1, 2, 17, 71]) {
+            expect(fizzbuzz(i)).toContain(`${i}\n`);
+        }
+    });
+
+    test("prints 'fizz' if number is multiple of 3'", () => {
+        for (let i of [3, 66, 90]) {
+            expect(fizzbuzz(i)).toContain("fizz\n");
+        }
+    });
+
+    test("prints 'buzz' if number is multiple of 5'", () => {
+        for (let i of [10, 50, 95, 100]) {
+            expect(fizzbuzz(i)).toContain("buzz\n");
+        }
+    });
+
+    test("prints 'fizzbuzz' if number is multiple of 3 and 5'", () => {
+        for (let i of [15, 30, 75]) {
+            expect(fizzbuzz(i)).toContain("fizzbuzz\n");
+        }
+    });
 });
--- fizzbuzz/ts/src/fizzbuzz.ts
+++ fizzbuzz/ts/src/fizzbuzz.ts
@@ -1,3 +1,20 @@
 export function fizzbuzz(value: number): string {
-    return "bar"
+    let result: string = "";
+    for(let i: number = 1; i <= 100; i++) {
+        if (isDivBy(i, 15)) {
+            result += "fizzbuzz\n";
+        } else if (isDivBy(i, 3)) {
+            result += "fizz\n";
+        } else if (isDivBy(i, 5)) {
+            result += "buzz\n";
+        } else {
+            result += `${i.toString()}\n`;
+        }
+    }
+
+    return result;
+}
+
+function isDivBy(value: number, divisor: number): boolean {
+    return value % divisor === 0;
 }
